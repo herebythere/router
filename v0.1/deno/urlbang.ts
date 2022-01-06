a9b43c7b2e6dcd5d2ca1343f4ff80733c6623017
Brian Taylor Vann <brian.t.vann@gmail.com>
2022-01-05 23:50:07 -0800

    fix(urlbang): remove unused type

--- v0.1/deno/urlbang.ts
+++ v0.1/deno/urlbang.ts
@@ -1,17 +1,8 @@
-interface PushHistoryParams {
-  url: string;
-  title: string;
-  kind: unknown;
-  state: unknown;
-}
-
-type PushHistory = (params: PushHistoryParams) => void;
-
 const DOMAIN = window.location.host;
-
-// can use postMessage as a fallback
 const rc = new BroadcastChannel(`${DOMAIN}:urlbang_reciever`);
 const bc = new BroadcastChannel(`${DOMAIN}:urlbang_broadcast`);
+
+// can use postMessage as a fallback
 
 window.addEventListener("popstate", (e: PopStateEvent) => {
   bc.postMessage(e.state);
