d8d40dbaf8990800d1929ef074671ddbac0d325a
Brian Taylor Vann <brian.t.vann@gmail.com>
2022-01-06 23:36:31 -0800

    feat(publisher): dom module labelled as history

--- docs/build-urlbang-demo.sh
+++ docs/build-urlbang-demo.sh
@@ -2,9 +2,9 @@
 
 current_dir=`dirname $0`
 bundled_pathname=$current_dir/../v0.1/bundled/urlbang.js
-bundled_dom_pathname=$current_dir/../v0.1/bundled/urlbang.dom.js 
+bundled_history_pathname=$current_dir/../v0.1/bundled/urlbang.history.js 
 scripts_dir=$current_dir/scripts/
 
 mkdir -p $scripts_dir
 cp $bundled_pathname $scripts_dir
-cp $bundled_dom_pathname $scripts_dir
+cp $bundled_history_pathname $scripts_dir
--- docs/scripts/urlbang.js
+++ docs/scripts/urlbang.js
@@ -1,15 +1,16 @@
 const DOMAIN = window.location.host;
 const bc = new BroadcastChannel(`${DOMAIN}:urlbang`);
-const broadcast = (message) => {
-  message.broadcast = true;
-  bc.postMessage(message);
+const broadcast = (message)=>{
+    message.broadcast = true;
+    bc.postMessage(message);
 };
-const addRecieverCallback = (callback) => {
-  const wrappedCallback = (e) => {
-    if (e.data.broadcast) return;
-    callback(e);
-  };
-  bc.addEventListener("message", wrappedCallback);
-  return () => bc.removeEventListener("message", wrappedCallback);
+const addRecieverCallback = (callback)=>{
+    const wrappedCallback = (e)=>{
+        if (e.data.broadcast) return;
+        callback(e);
+    };
+    bc.addEventListener("message", wrappedCallback);
+    return ()=>bc.removeEventListener("message", wrappedCallback)
+    ;
 };
 export { addRecieverCallback as addRecieverCallback, broadcast as broadcast };
