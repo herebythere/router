b4e48e144340998539a4f986fb2c2993bdb735d2
taylor <brian.t.vann@gmail.com>
2024-02-20 10:49:40 -0800

    fmt

--- nodejs/v0.1/src/router/router.ts
+++ nodejs/v0.1/src/router/router.ts
@@ -1,19 +1,24 @@
-import type { BroadcasterInterface, MinimalRouterState } from "../type_flyweight/router.ts";
+import type {
+  BroadcasterInterface,
+  MinimalRouterState,
+} from "../type_flyweight/router.ts";
 
 const ROUTER = "router";
 const EMPTY = "";
 // so much is reliant on lifecycle methods and on "history change"
 
-function push<S extends MinimalRouterState>(broadcaster: BroadcasterInterface, message: S) {
+function push<S extends MinimalRouterState>(
+  broadcaster: BroadcasterInterface,
+  message: S,
+) {
   history.pushState(message, EMPTY, message.location);
-	document.title = message.title;
-	broadcaster.postMessage(history.state);
+  document.title = message.title;
+  broadcaster.postMessage(history.state);
 }
 
-
 function onHistoryChange(broadcaster: BroadcasterInterface) {
-	document.title = history.state.title;
-	broadcaster.postMessage(history.state);
+  document.title = history.state.title;
+  broadcaster.postMessage(history.state);
 }
 
 export { push, onHistoryChange };
